a2c1d315ba46d62e0b94e231427336b60c61e081
starkoracia <starkoracia@gmail.com>
2021-10-17 22:31:21 +0300

    + semantic fixes

--- hw_2_strings/src/main/java/ua/com/alevel/StringReverser.java
+++ hw_2_strings/src/main/java/ua/com/alevel/StringReverser.java
@@ -6,16 +6,18 @@ import java.util.regex.Pattern;
 /**
  * Util class, for work with strings.
  * It has four, reverse() method implementations, which return String.
- * */
+ */
 public final class StringReverser {
 
-    private StringReverser() {}
+    private StringReverser() {
+    }
 
-/**
- * Main simple reverse method that takes String and return same full reverse String.
- * @param src any String, which you want to reverse.
- * @return reversed Sting.
- * */
+    /**
+     * Main simple reverse method that takes String and return same full reverse String.
+     *
+     * @param src any String, which you want to reverse.
+     * @return reversed Sting.
+     */
     public static String reverse(String src) {
         StringBuilder reversedOutString = new StringBuilder("");
 
@@ -29,9 +31,10 @@ public final class StringReverser {
     /**
      * Implementing a reverse method, that takes a String and reverses the individual words
      * in the string, remaining all spaces, on their places.
-     * @param src any String, which you want to reverse.
+     *
+     * @param src            any String, which you want to reverse.
      * @param isWordsReverse - if true - reverses only words, if false, work same {@link StringReverser#reverse(String)}
-     * */
+     */
     public static String reverse(String src, boolean isWordsReverse) {
         if (isWordsReverse) {
             StringBuilder outString = new StringBuilder("");
@@ -66,11 +69,15 @@ public final class StringReverser {
     public static String reverse(String src, int firstIndex, int lastIndex) {
         StringBuilder outString = new StringBuilder("");
 
-        if (lastIndex > (src.length())) lastIndex = src.length();
-        if (firstIndex > (src.length())) firstIndex = src.length();
-        if (lastIndex < 0) lastIndex = 0;
-        if (firstIndex < 0) firstIndex = 0;
-        if(firstIndex > lastIndex) {
+        if (lastIndex > (src.length()))
+            lastIndex = src.length();
+        if (firstIndex > (src.length()))
+            firstIndex = src.length();
+        if (lastIndex < 0)
+            lastIndex = 0;
+        if (firstIndex < 0)
+            firstIndex = 0;
+        if (firstIndex > lastIndex) {
             int temp = firstIndex;
             firstIndex = lastIndex;
             lastIndex = temp;
