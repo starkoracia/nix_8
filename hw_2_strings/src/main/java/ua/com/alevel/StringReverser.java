e16411c142239c17a77236c98cea1d1b6c3a9f96
starkoracia <starkoracia@gmail.com>
2021-10-18 18:05:35 +0300

    + fix pom project name + add reverse method for index with bool isWordsReverse

--- hw_2_strings/src/main/java/ua/com/alevel/StringReverser.java
+++ hw_2_strings/src/main/java/ua/com/alevel/StringReverser.java
@@ -17,7 +17,7 @@ public final class StringReverser {
      *
      * @param src any String, which you want to reverse.
      * @return reversed Sting.
-     */
+     * */
     public static String reverse(String src) {
         StringBuilder reversedOutString = new StringBuilder("");
 
@@ -32,9 +32,10 @@ public final class StringReverser {
      * Implementing a reverse method, that takes a String and reverses the individual words
      * in the string, remaining all spaces, on their places.
      *
-     * @param src            any String, which you want to reverse.
+     * @param src any String, which you want to reverse.
      * @param isWordsReverse - if true - reverses only words, if false, work same {@link StringReverser#reverse(String)}
-     */
+     * @return returns reversed String, according to method description.
+     * */
     public static String reverse(String src, boolean isWordsReverse) {
         if (isWordsReverse) {
             StringBuilder outString = new StringBuilder("");
@@ -52,6 +53,14 @@ public final class StringReverser {
         return reverse(src);
     }
 
+    /**
+     * Implementing a reverse method, that takes a src String and destString as second param,
+     * and reverses all matches of destString in src String.
+     *
+     * @param src any String, which you want to reverse.
+     * @param destString all matches with this param, will be reverse in src String.
+     * @return returns reversed String, according to method description.
+     * */
     public static String reverse(String src, String destString) {
         StringBuilder outString = new StringBuilder("");
         String[] words = src.split(destString);
@@ -66,7 +75,32 @@ public final class StringReverser {
         return outString.toString();
     }
 
+    /**
+     * Implementing a reverse method, that takes a src String and two int values, such as firstIndex and lastIndex, as
+     * the range in which the string will be processed by reverse method. Both of index params, could be first ore last
+     * index, depending on which index will be bigger.
+     * If one of index param will go beyond the string, will be takes the closest extreme index value.
+     * @param src any String, which you want to reverse.
+     * @param firstIndex the range in which the string will be processed by reverse method.
+     * @param lastIndex the range in which the string will be processed by reverse method.
+     * @return returns reversed String, according to method description.
+     * */
     public static String reverse(String src, int firstIndex, int lastIndex) {
+        return reverse(src, firstIndex, lastIndex, false);
+    }
+
+    /**
+     * Implementing a reverse method, that takes a src String and two int values, such as firstIndex and lastIndex, as
+     * the range in which the string will be processed by reverse method. Both of index params, could be first ore last
+     * index, depending on which index will be bigger.
+     * If one of index param will go beyond the src string index, will be takes the closest extreme index value.
+     * @param src any String, which you want to reverse.
+     * @param firstIndex the range in which the string will be processed by reverse method.
+     * @param lastIndex the range in which the string will be processed by reverse method.
+     * @param isWordsReverse if true - reverses only words, if false, work same {@link StringReverser#reverse(String, int, int)}
+     * @return returns reversed String, according to method description.
+     * */
+    public static String reverse(String src, int firstIndex, int lastIndex, boolean isWordsReverse) {
         StringBuilder outString = new StringBuilder("");
 
         if (lastIndex > (src.length()))
@@ -84,8 +118,9 @@ public final class StringReverser {
         }
         return outString
                 .append(src, 0, firstIndex)
-                .append(reverse(src.substring(firstIndex, lastIndex), true))
+                .append(reverse(src.substring(firstIndex, lastIndex), isWordsReverse))
                 .append(src.substring(lastIndex))
                 .toString();
     }
+
 }
